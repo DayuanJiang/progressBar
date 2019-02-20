78b32f159b58386fa1fe4c1fad9c4dde0a1442f9
Jiang <jiang@mac.local>
2019-02-20 22:53:32 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='console-progress-bar',
+      version='0.1',
+      description='Display a progressBar in the console',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+      ],
+      keywords='function progress bar console',
+      url='www.jiang.jp',
+      author='Jiang',
+      author_email='me@jiang.jp',
+      license='MIT',
+      packages=['progressBar'],
+      zip_safe=False)
+
